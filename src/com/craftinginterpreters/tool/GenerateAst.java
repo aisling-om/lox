5a03fe25434c6e452fc40cf69757070a343bb6c2
aongus <aongus.omurchadha@gmail.com>
2020-02-11 20:57:52 -0500

    Update GenerateAst.java
    Finish 5.2

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -1,6 +1,7 @@
 package com.craftinginterpreters.tool;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,6 +23,47 @@ public class GenerateAst {
     }
 
     private static void defineAst( String outputDir, String basename, List<String> types) throws IOException {
+        String path = outputDir + "/" + basename + ".java";
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
 
+        writer.println("package com.craftinginterpreters.lox;");
+        writer.println();
+        writer.println("import java.util.list");
+        writer.println();
+        writer.println("abstract class " + basename + " {");
+
+        // the AST classes
+        for (String type: types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineType(writer, basename, className, fields);
+        }
+
+        writer.println("}");
+        writer.close();
+    }
+
+    private static void defineType( PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("    static class " + className + " extends " + baseName + " {");
+
+        //Constructor
+        writer.println("    " + className + "(" + fieldList + ") {");
+
+        // Store parameters in fields
+        String[] fields = fieldList.split(", ");
+        for (String field : fields) {
+            String name = field.split(" ")[1];
+            writer.println("    this." + name + " = " + name + ";");
+        }
+
+        writer.println("    }");
+
+        //Fields
+        writer.println();
+        for (String field : fields) {
+            writer.println("    final " + field + ";");
+        }
+
+        writer.println("    }");
     }
 }
